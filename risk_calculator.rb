dd19c67042ce32b5d60a4e33975d6f7eb6b2ec82
Dan <dansification@gmail.com>
2019-08-06 18:22:49 -0400

    Be able set ATR Ratio and Risk Percentage

--- risk_calculator.rb
+++ risk_calculator.rb
@@ -2,24 +2,35 @@ class RiskCalculator
   ATR_RATIO = 1.5
   RISK_PERCENTAGE = 0.02
 
+  private_constant :ATR_RATIO, :RISK_PERCENTAGE
+  attr_writer :atr_ratio, :risk_percentage
+
   def initialize(balance:, atr:)
     @balance = balance
     @atr = atr
   end
 
   def pip_value
-    (risk / (ATR_RATIO * atr)).round(2)
+    (risk / (atr_ratio * atr)).round(2)
   end
 
   def risk
-    balance * RISK_PERCENTAGE
+    balance * risk_percentage
   end
 
   def pips
-    ATR_RATIO * atr
+    atr_ratio * atr
   end
 
   private
 
   attr_reader :atr, :balance
+
+  def atr_ratio
+    @atr_ratio ||= ATR_RATIO
+  end
+
+  def risk_percentage
+    @risk_percentage ||= RISK_PERCENTAGE
+  end
 end
